b325e441fb31aa3b53dabf1b33581f4bc2764931
makmu <makmu@users.noreply.github.com>
2016-02-25 10:47:00 -0800

    add unit test project

---
+++ OutlookMatters.Test/MailItemContextMenuEntryTest.cs
@@ -0,0 +1,19 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace OutlookMatters.Test
+{
+    [TestFixture]
+    public class MailItemContextMenuEntryTest
+    {
+        [Test]
+        public void GetCustomUI_ReturnsCustomUiForExplorer()
+        {
+            var classUnderTest = new MailItemContextMenuEntry();
+
+            var result = classUnderTest.GetCustomUI("Microsoft.Outlook.Explorer");
+
+            result.Should().NotBeEmpty("because there should be custom UI xml for the outlook explorer");
+        }
+    }
+}
