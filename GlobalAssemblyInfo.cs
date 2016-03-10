c47759371285c9ecc8eb676890f23d5a5193f757
makmu <makmu@users.noreply.github.com>
2016-03-10 10:41:48 +0100

    introduce global assembly info and general versioning

---
+++ GlobalAssemblyInfo.cs
@@ -0,0 +1,31 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyCompany("outlook-matters project")]
+[assembly: AssemblyProduct("OutlookMatters Addin")]
+[assembly: AssemblyDescription("An Outlook-Mattermost Bridge")]
+[assembly: AssemblyCopyright("Copyright © 2016 by the outlook-matters developers")]
+[assembly: AssemblyTrademark("This application has no Trademark.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyVersion(Version.Current)]
+[assembly: AssemblyFileVersion(Version.Current)]
+[assembly: AssemblyInformationalVersion(Version.FullCurrent)]
+
+class Version
+{
+    // refer to http://semver.org for more information
+    public const string Major = "1";
+    public const string Minor = "0";
+    public const string Patch = "0";
+
+    public const string Label = ReleaseLabel.Dev;
+    public const string AdditionalReleaseInformation = "";
+    public const string Current = Major + "." + Minor + "." + Patch;
+    public const string FullCurrent = Current + "-" + Label + "." + AdditionalReleaseInformation;
+}
+
+class ReleaseLabel
+{
+    public const string Dev = "dev";
+    public const string ReleaseCandidate = "rc";
+    public const string Final = "official";
+}
--- OutlookMatters/Properties/AssemblyInfo.cs
+++ OutlookMatters/Properties/AssemblyInfo.cs
@@ -7,12 +7,6 @@ using System.Security;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("OutlookMatters")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("OutlookMatters")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
-[assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
@@ -33,6 +27,4 @@ using System.Security;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
 
