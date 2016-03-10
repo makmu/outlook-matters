14cec4848d0d6882b15b560fd6d088aa45a4ae01
makmu <makmu@users.noreply.github.com>
2016-03-10 12:29:48 +0100

    release commit v1.0.0

--- GlobalAssemblyInfo.cs
+++ GlobalAssemblyInfo.cs
@@ -17,7 +17,7 @@ class Version
     public const string Minor = "0";
     public const string Patch = "0";
 
-    public const string Label = ReleaseLabel.Dev;
+    public const string Label = ReleaseLabel.Final;
     public const string AdditionalReleaseInformation = "";
     public const string Current = Major + "." + Minor + "." + Patch;
     public const string FullCurrent = Current + "-" + Label + AdditionalReleaseInformation;
