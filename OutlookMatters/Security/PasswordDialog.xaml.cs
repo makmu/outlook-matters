69a37f60649572f641f516bcfdd9ceec525347e3
makmu <makmu@users.noreply.github.com>
2016-03-27 14:30:15 +0200

    wrap password dialog into shell so that it can be opened more than once

--- OutlookMatters/Security/PasswordDialog.xaml.cs
+++ OutlookMatters/Security/PasswordDialog.xaml.cs
@@ -6,22 +6,11 @@ namespace OutlookMatters.Security
     /// <summary>
     ///     Interaction logic for PasswordDialog.xaml
     /// </summary>
-    public partial class PasswordDialog : IPasswordProvider
+    public partial class PasswordDialog
     {
         public PasswordDialog()
         {
             InitializeComponent();
-        }
-
-        public string GetPassword(string username)
-        {
-            Username.Content = username;
-            ShowDialog();
-            if (DialogResult == true)
-            {
-                return Password.Password;
-            }
-            throw new UserAbortException("cannot provide password for '" + username + "': user abort");
         }
 
         private void OnOkClick(object sender, RoutedEventArgs e)
