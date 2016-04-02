3fc813db1134e87ff48b0956c0d08251d1ad66d2
makmu <makmu@users.noreply.github.com>
2016-04-02 11:32:03 +0200

    improve code coverage

---
+++ OutlookMatters.Test/Settings/SettingsTest.cs
@@ -0,0 +1,55 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace OutlookMatters.Test.Settings
+{
+    [TestFixture]
+    public class SettingsTest
+    {
+        [Test]
+        [TestCase("", "", "", "", true)]
+        [TestCase("x", "", "", "", false)]
+        [TestCase("", "x", "", "", false)]
+        [TestCase("", "", "x", "", false)]
+        [TestCase("", "", "", "x", false)]
+        public void Equals_ChecksAllMembers(string urlModifier, string teamIdModifier, string channelIdModifier,
+            string usernameModifier, bool expected)
+        {
+            const string url = "http://tempuri.org";
+            const string teamId = "team id";
+            const string channelId = "channel id";
+            const string username = "user name";
+            var otherSettings = new OutlookMatters.Settings.Settings(url + urlModifier, teamId + teamIdModifier,
+                channelId + channelIdModifier, username + usernameModifier);
+            var classUnderTest = new OutlookMatters.Settings.Settings(url, teamId, channelId, username);
+
+            var result = classUnderTest.Equals(otherSettings);
+
+            result.Should().Be(expected);
+        }
+
+        [Test]
+        [TestCase(null)]
+        [TestCase("")]
+        public void Equals_ReturnsFalse_IfObjectTypeDoesNotMatch(object other)
+        {
+            var classUnderTest = new OutlookMatters.Settings.Settings(string.Empty, string.Empty, string.Empty,
+                string.Empty);
+
+            var result = classUnderTest.Equals(other);
+
+            result.Should().BeFalse();
+        }
+
+        [Test]
+        public void Equals_ReturnsTrue_IfReferenceEqual()
+        {
+            var classUnderTest = new OutlookMatters.Settings.Settings(string.Empty, string.Empty, string.Empty,
+                string.Empty);
+
+            var result = classUnderTest.Equals(classUnderTest);
+
+            result.Should().BeTrue();
+        }
+    }
+}
---
+++ OutlookMatters.Test/Utils/UserAbortExceptionTest.cs
@@ -0,0 +1,21 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using OutlookMatters.Utils;
+
+namespace OutlookMatters.Test.Utils
+{
+    [TestFixture]
+    public class UserAbortExceptionTest
+    {
+        [Test]
+        public void Test()
+        {
+            const string message = "message";
+            var classUnderTest = new UserAbortException(message);
+
+            var result = classUnderTest.Message;
+
+            result.Should().Be(message);
+        }
+    }
+}
