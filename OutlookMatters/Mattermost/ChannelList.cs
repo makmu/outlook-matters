c3bd41890ab686361f458a6a43f79137458321b5
maxlmo <maxlmo@users.noreply.github.com>
2016-04-07 14:52:33 +0200

    Split ChannelList into two seperate files

--- OutlookMatters/Mattermost/ChannelList.cs
+++ OutlookMatters/Mattermost/ChannelList.cs
@@ -7,16 +7,4 @@ namespace OutlookMatters.Mattermost
         [JsonProperty("channels")]
         public List<Channel> Channels { get; set; }
     }
-
-    public class Channel
-    {
-        [JsonProperty("id")]
-        public string ChannelId { get; set; }
-
-        [JsonProperty("display_name")]
-        public string ChannelName { get; set; }
-
-        [JsonProperty("type")]
-        public string Type { get; set; }
-    }
 }
