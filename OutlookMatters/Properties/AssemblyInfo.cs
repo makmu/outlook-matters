5717a4c1c95755915b21df831fbc4ae6daab15aa
makmu <makmu@users.noreply.github.com>
2016-04-11 19:12:04 +0200

    fix and clean up assembly info and references

--- OutlookMatters/Properties/AssemblyInfo.cs
+++ OutlookMatters/Properties/AssemblyInfo.cs
@@ -17,14 +17,3 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 
 [assembly: Guid("93da7668-a237-47f0-a9d6-77dac59a7211")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
