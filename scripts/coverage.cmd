01f1f7557dd2e9aef838a26aaa9eedabb334d247
maxlmo <maxlmo@users.noreply.github.com>
2016-04-11 16:38:22 +0200

    Adjust path for new project

--- scripts/coverage.cmd
+++ scripts/coverage.cmd
@@ -1,1 +1,1 @@
-.\packages\OpenCover.4.6.519\tools\OpenCover.Console.exe -target:.\packages\NUnit.ConsoleRunner.3.2.0\tools\nunit3-console.exe -targetargs:".\OutlookMatters.Test\bin\Debug\OutlookMatters.Test.dll" -filter:"+[*]OutlookMatters* -[*Test*]*" -register:user
+.\packages\OpenCover.4.6.519\tools\OpenCover.Console.exe -target:.\packages\NUnit.ConsoleRunner.3.2.0\tools\nunit3-console.exe -targetargs:".\OutlookMatters.Core.Test\bin\Debug\OutlookMatters.Core.Test.dll" -filter:"+[*]OutlookMatters* -[*Test*]*" -register:user
