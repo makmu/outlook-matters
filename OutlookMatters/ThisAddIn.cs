c4e14bfb09d6db478463ae88369b05680b8d35f8
maxlmo <maxlmo@users.noreply.github.com>
2016-06-02 13:37:03 +0200

    Add client factory

--- OutlookMatters/ThisAddIn.cs
+++ OutlookMatters/ThisAddIn.cs
@@ -9,7 +9,6 @@ using OutlookMatters.Core.Error;
 using OutlookMatters.Core.Http;
 using OutlookMatters.Core.Mail;
 using OutlookMatters.Core.Mattermost;
-using OutlookMatters.Core.Mattermost.v1;
 using OutlookMatters.Core.Reply;
 using OutlookMatters.Core.Security;
 using OutlookMatters.Core.Session;
@@ -53,12 +52,11 @@ namespace OutlookMatters
         protected override Office.IRibbonExtensibility CreateRibbonExtensibilityObject()
         {
             var httpClient = new DotNetHttpClient();
-            var restService = new HttpRestService(httpClient);
-            var mattermost = new HttpClient(new HttpSessionFactory(restService), restService);
+            var clientFactory = new MattermostClientFactory(httpClient);
             var passwordDialog = new PasswordDialogShell();
             var caches = new CompositeCache();
             var settingsService = new ApplicationSettingsService(caches);
-            var sessionRepository = new SingleSignOnSessionRepository(mattermost, settingsService, passwordDialog);
+            var sessionRepository = new SingleSignOnSessionRepository(clientFactory, settingsService, passwordDialog);
             caches.Add(sessionRepository);
             var explorerService = new OutlookExplorerService();
             var mailExplorer = new OutlookMailExplorer(explorerService);
@@ -90,5 +88,7 @@ namespace OutlookMatters
         }
 
         #endregion
+
+        
     }
 }
