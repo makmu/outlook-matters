519a8a722ba495da8083a29cb51df392dab24390
maxlmo <maxlmo@users.noreply.github.com>
2016-06-02 13:52:09 +0200

    Implement method to fetch channels for REST API v3

--- OutlookMatters.Core/Mattermost/v3/ChatFactoryImpl.cs
+++ OutlookMatters.Core/Mattermost/v3/ChatFactoryImpl.cs
@@ -1,13 +1,19 @@
 ﻿using System;
 using OutlookMatters.Core.Chat;
+using OutlookMatters.Core.Mattermost.v3.Interface;
 
 namespace OutlookMatters.Core.Mattermost.v3
 {
-    public class ChatFactoryImpl : IChatFactory
+    public class ChatFactoryImpl : IChatFactory, IChatChannelFactory
     {
-        public ISession NewInstance(Uri uri, string token, string userId)
+        public ISession NewInstance(IRestService restService, Uri uri, string token, string userId, string teamId)
         {
-            throw new NotImplementedException();
+            return new SessionImpl(restService, uri, token, userId, teamId, this);
+        }
+
+        public IChatChannel NewInstance(IRestService restService, Uri baseUri, string token, string userId, string teamId, Channel channel)
+        {
+            return new ChatChannelImpl(restService, baseUri, token, userId, teamId, channel);
         }
     }
 }
--- OutlookMatters.Core/Mattermost/v3/IChatFactory.cs
+++ OutlookMatters.Core/Mattermost/v3/IChatFactory.cs
@@ -1,10 +1,11 @@
 using System;
 using OutlookMatters.Core.Chat;
+using OutlookMatters.Core.Mattermost.v3.Interface;
 
 namespace OutlookMatters.Core.Mattermost.v3
 {
     public interface IChatFactory
     {
-        ISession NewInstance(Uri uri, string token, string userId);
+        ISession NewInstance(IRestService restService, Uri uri, string token, string userId, string teamId);
     }
 }
