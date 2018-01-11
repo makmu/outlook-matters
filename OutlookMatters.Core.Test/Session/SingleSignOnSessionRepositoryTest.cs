daaf69867a1c63b114a1002f2a70476aab996c38
Rebar Hassan <rebar.hassan@astrum-it.de>
2018-01-11 12:40:47 +0100

    Implement unit tests.

--- OutlookMatters.Core.Test/Session/SingleSignOnSessionRepositoryTest.cs
+++ OutlookMatters.Core.Test/Session/SingleSignOnSessionRepositoryTest.cs
@@ -1,11 +1,15 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
 using FluentAssertions;
 using Moq;
 using NUnit.Framework;
 using OutlookMatters.Core.Chat;
+using OutlookMatters.Core.Http;
 using OutlookMatters.Core.Security;
 using OutlookMatters.Core.Session;
 using OutlookMatters.Core.Settings;
+using OutlookMatters.Core.Utils;
 
 namespace Test.OutlookMatters.Core.Session
 {
@@ -16,13 +20,15 @@ namespace Test.OutlookMatters.Core.Session
         public async Task RestoreSession_ReturnsNewSessionFromClient()
         {
             var settingsLoadService = new Mock<ISettingsLoadService>();
-            var settings = new AddInSettings("myUrl", "testTeamId", "Donald Duck", "channels", It.IsAny<MattermostVersion>());
+            var settings = new AddInSettings("myUrl", "testTeamId", "Donald Duck", "channels",
+                It.IsAny<MattermostVersion>());
             settingsLoadService.Setup(x => x.Load()).Returns(settings);
             var mattermost = new Mock<IClient>();
             var clientFactory = SetupClientFactoryMock(mattermost);
             var session = new Mock<ISession>();
             mattermost.Setup(
-                x => x.LoginByUsername(settings.MattermostUrl, settings.TeamId, settings.Username, It.IsAny<string>()))
+                    x => x.LoginByUsername(settings.MattermostUrl, settings.TeamId, settings.Username,
+                        It.IsAny<string>()))
                 .Returns(session.Object);
             var classUnderTest = new SingleSignOnSessionRepository(clientFactory.Object,
                 settingsLoadService.Object,
@@ -45,7 +51,7 @@ namespace Test.OutlookMatters.Core.Session
             var session = new Mock<ISession>();
             var clientFactory = SetupClientFactoryMock(mattermost);
             mattermost.Setup(
-                x => x.LoginByUsername(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>(), password))
+                    x => x.LoginByUsername(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>(), password))
                 .Returns(session.Object);
             var classUnderTest = new SingleSignOnSessionRepository(clientFactory.Object,
                 DefaultSettingsLoadService,
@@ -66,7 +72,8 @@ namespace Test.OutlookMatters.Core.Session
             var session2 = new Mock<ISession>();
             var clientFactory = SetupClientFactoryMock(mattermost);
             mattermost.SetupSequence(
-                x => x.LoginByUsername(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>()))
+                    x => x.LoginByUsername(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>(),
+                        It.IsAny<string>()))
                 .Returns(session1.Object)
                 .Returns(session2.Object);
             var classUnderTest = new SingleSignOnSessionRepository(clientFactory.Object,
@@ -83,17 +90,73 @@ namespace Test.OutlookMatters.Core.Session
         }
 
         [Test]
+        public async Task RestoreSession_RetriesLogin_IfWebExceptionIsTrustFailureAndUserContinues()
+        {
+            var mattermost = new Mock<IClient>();
+            var session = new Mock<ISession>();
+            var clientfactory = SetupClientFactoryMock(mattermost);
+            var exception = new WebException("", new Exception("invalid ssl"), WebExceptionStatus.TrustFailure, It.IsAny<HttpWebResponse>() );
+            mattermost.SetupSequence(
+                x => x.LoginByUsername(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>(),
+                    It.IsAny<string>()))
+                    .Throws(exception)
+                    .Returns(session.Object);
+            var settingsLoadService = new Mock<ISettingsLoadService>();
+            var settings = new AddInSettings("myUrl", "testTeamId", "Donald Duck", "channels",
+                MattermostVersion.ApiVersionFour);
+            settingsLoadService.Setup(x => x.Load()).Returns(settings);
+            var invalidSslQuestion = new Mock<ITrustInvalidSslQuestion>();
+            invalidSslQuestion.Setup(x => x.GetAnswer(settings.MattermostUrl, exception.InnerException.Message)).Returns(true);
+            var cut = new SingleSignOnSessionRepository(clientfactory.Object, settingsLoadService.Object,
+                new Mock<IPasswordProvider>().Object, invalidSslQuestion.Object,
+                new Mock<IServerCertificateValidator>().Object);
+
+            var result = await cut.RestoreSession();
+
+            result.ShouldBeEquivalentTo(session.Object);
+        }
+
+        [Test]
+        public void RestoreSession_ThrowsUserAbortException_IfUserAbortsOnTrustFailureException()
+        {
+            var mattermost = new Mock<IClient>();
+            var session = new Mock<ISession>();
+            var clientfactory = SetupClientFactoryMock(mattermost);
+            var exception = new WebException("", new Exception("invalid ssl"), WebExceptionStatus.TrustFailure, It.IsAny<HttpWebResponse>() );
+            mattermost.SetupSequence(
+                x => x.LoginByUsername(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>(),
+                    It.IsAny<string>()))
+                    .Throws(exception)
+                    .Returns(session.Object);
+            var settingsLoadService = new Mock<ISettingsLoadService>();
+            var settings = new AddInSettings("myUrl", "testTeamId", "Donald Duck", "channels",
+                MattermostVersion.ApiVersionFour);
+            settingsLoadService.Setup(x => x.Load()).Returns(settings);
+            var invalidSslQuestion = new Mock<ITrustInvalidSslQuestion>();
+            invalidSslQuestion.Setup(x => x.GetAnswer(settings.MattermostUrl, exception.InnerException.Message)).Returns(false);
+            var cut = new SingleSignOnSessionRepository(clientfactory.Object, settingsLoadService.Object,
+                new Mock<IPasswordProvider>().Object, invalidSslQuestion.Object,
+                new Mock<IServerCertificateValidator>().Object);
+
+            Func<Task> func = async () => { await cut.RestoreSession(); };
+
+            func.ShouldThrow<UserAbortException>();
+        }
+
+        [Test]
         public async Task RestoreSession_CreatesNewSession_IfCacheInvalidated()
         {
             var mattermost = new Mock<IClient>();
             var session1 = new Mock<ISession>();
             var session2 = new Mock<ISession>();
             var settingsLoadService = new Mock<ISettingsLoadService>();
-            var settings = new AddInSettings("myUrl", "testTeamId", "Donald Duck", "channels", MattermostVersion.ApiVersionFour);
+            var settings = new AddInSettings("myUrl", "testTeamId", "Donald Duck", "channels",
+                MattermostVersion.ApiVersionFour);
             settingsLoadService.Setup(x => x.Load()).Returns(settings);
             var clientFactory = SetupClientFactoryMock(mattermost);
             mattermost.SetupSequence(
-                x => x.LoginByUsername(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>()))
+                    x => x.LoginByUsername(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>(),
+                        It.IsAny<string>()))
                 .Returns(session1.Object)
                 .Returns(session2.Object);
             var classUnderTest = new SingleSignOnSessionRepository(clientFactory.Object,
